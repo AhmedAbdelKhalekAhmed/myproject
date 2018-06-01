9941171374f8e4a3324578543c6805a0136d26be
a7med3bdel5alek <a7med3bdel5alek@gmail.com>
2018-06-01 06:05:37 +0200

    make it better

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,14 +1,37 @@
 @if(count($errors)>0)
- @foreach($errors->all() as $error)
-  {{$error}}
- @endforeach
-@endif
+  @foreach($errors->all() as $error)
+  
+  <div class="alert alert-dismissible alert-danger">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Oh  !</strong> {{$error}}  and try submitting again.
+</div>
+  
+  
+  
+  @endforeach
+
+  @endif
+
+  @if(session('success'))
+  
+<div class="alert alert-dismissible alert-success">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Well done!</strong>   {{session('success')}}.
+</div>
+  
 
 
-@if(session('success'))
-  {{session('success')}}
-@endif
+  @endif
 
-@if(session('erorr'))
-  {{session('erorr')}}
-@endif
+
+   @if(session('error'))
+  
+  <div class="alert alert-dismissible alert-danger">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Oh  !</strong>   {{session('error')}}  and try submitting again.
+</div>
+
+
+   
+
+  @endif
